80d8c01f6fa966bf070ae11f6e865345f32ed5b7
Xenia Litau <ksuschalitau@gmail.com>
2024-12-21 16:01:05 +0100

    Reciept.js

---
+++ Reciept.js
@@ -0,0 +1,14 @@
+
+console.log("=========================================");
+console.log("            LUIGI'S PIZZERIA ");
+console.log("-----------------------------------------");
+console.log("              Order Details  ");
+console.log("-----------------------------------------");
+console.log(" Item                Quantity    Price ");
+console.log("-----------------------------------------");
+console.log(" Pepperoni Pizza         1         10.90");
+console.log(" Garlic Bread            5          6.90");
+console.log(" Coca Cola               2          3.90");
+console.log("-----------------------------------------");
+console.log(" Total                              21,70")
+console.log("=========================================");
