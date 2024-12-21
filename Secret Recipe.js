49bc8795d305626e996acd4c7864b5a8f9737d2d
Xenia Litau <ksuschalitau@gmail.com>
2024-12-21 15:46:12 +0100

    Secret Recipe.js

---
+++ Secret Recipe.js
@@ -0,0 +1,8 @@
+
+// Write code below 💖
+
+console.log("My favorite dish: Pelmeni");
+console.log("     Ingredients:");
+console.log("  1. Pork ground meat")
+console.log("  2. Dough made out of water, eggs, flour and salt")
+// Secret Ingredient I will be using is lots of love frfr
