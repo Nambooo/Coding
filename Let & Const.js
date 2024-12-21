fa1b04a90bf0ceb5e9f0d8017ffcabfe7f3d5dd0
Xenia Litau <ksuschalitau@gmail.com>
2024-12-21 16:16:57 +0100

    Let & Const.js

---
+++ Let & Const.js
@@ -0,0 +1,13 @@
+// Write code below 💖
+
+let Location = "Germany";
+  Location = "America";
+let Mood = "Neutral";
+
+const Name = "Xenia";
+const Color = "Indigo";
+
+console.log(Location);
+console.log(Mood);
+console.log(Name);
+console.log(Color);
