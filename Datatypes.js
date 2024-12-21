e74051be95bc48a2148743c2d77d2294b7ad3025
Xenia Litau <ksuschalitau@gmail.com>
2024-12-21 16:33:36 +0100

    Datatypes.js

---
+++ Datatypes.js
@@ -0,0 +1,11 @@
+// Write code below 💖
+
+let companyName = 'Codedex';
+const foundingYear = 2021;
+let isActive = true;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
