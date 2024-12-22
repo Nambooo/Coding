be6fd8d6330113f9a89161463779114247bffc8a
Xenia Litau <ksuschalitau@gmail.com>
2024-12-22 20:04:50 +0100

    Update 42_async_and_await.js

--- 42_async_and_await.js
+++ 42_async_and_await.js
@@ -1,7 +1,8 @@
 async function fetchTriviaQuestions() {
     const response =  await fetch("https://opentdb.com/api.php?amount=10"); 
-      if (response.ok === false) {
+      if (!response.ok === false) {
         throw new Error("Failed to get trivia questions");
       }
       const data = await response.json();
+  return data.results();
     }
